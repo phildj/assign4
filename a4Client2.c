fc4bfdb3e086fe7317691dd9223a60bee5ea9cb1
phildj <33578252+phildj@users.noreply.github.com>
2017-12-03 20:59:12 -0600

    Add files via upload

---
+++ a4Client2.c
@@ -0,0 +1,100 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <time.h>
+
+#define MAXLINE 4096 /*max text line length*/
+//#define SERV_PORT 10010 /*port*/
+
+int
+main(int argc, char **argv) 
+{
+ int sockfd;
+ struct sockaddr_in servaddr;
+ char sendline[MAXLINE] = "";
+ char recvline[MAXLINE] = "";
+  FILE *f;
+  f = fopen("a4Client2Log.txt", "w");
+ // alarm(300);  // to terminate after 300 seconds
+	
+ //basic check of the arguments
+ //additional checks can be inserted
+ if (argc !=3) {
+  perror("Usage: TCPClient <Server IP> <Server Port>"); 
+  exit(1);
+ }
+	
+ //Create a socket for the client
+ //If sockfd<0 there was an error in the creation of the socket
+ if ((sockfd = socket (AF_INET, SOCK_STREAM, 0)) <0) {
+  perror("Problem in creating the socket");
+  exit(2);
+ }
+	
+ //Creation of the socket
+ memset(&servaddr, 0, sizeof(servaddr));
+ servaddr.sin_family = AF_INET;
+ servaddr.sin_addr.s_addr= inet_addr(argv[1]);
+ servaddr.sin_port =  htons((int) strtol(argv[2], (char **)NULL, 10)); 
+ //servaddr.sin_port =  htons(SERV_PORT); //convert to big-endian order
+	
+ //Connection of the client to the socket 
+ if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr))<0) {
+  perror("Problem in connecting to the server");
+  exit(3);
+ }
+ f = fopen("a4Client2Log.txt", "a");
+ while (1) {
+ strcat(sendline, "update classics set author = '*** UNKNOWN ***'  where author = 'Mark Twain'");
+  send(sockfd, sendline, strlen(sendline), 0);		
+  if (recv(sockfd, recvline, MAXLINE,0) == 0){
+   //error: server terminated prematurely
+   perror("The server terminated prematurely"); 
+   exit(4);
+  }
+  strcpy(sendline,"");
+  strcat(sendline, "select * from classics");
+  send(sockfd, sendline, strlen(sendline), 0);	
+  if (recv(sockfd, recvline, MAXLINE,0) == 0){
+   //error: server terminated prematurely
+   perror("The server terminated prematurely"); 
+   exit(4);
+  }
+  fprintf(f, "%s\n", recvline);
+  fputs(recvline, stdout);
+  printf("\n");
+  sleep(2);
+  strcpy(sendline,"");
+  strcat(sendline, "update classics set author = 'Mark Twain'  where author = '*** UNKNOWN ***'");
+  send(sockfd, sendline, strlen(sendline), 0);
+  if (recv(sockfd, recvline, MAXLINE,0) == 0){
+   //error: server terminated prematurely
+   perror("The server terminated prematurely"); 
+   exit(4);
+  }
+  strcpy(sendline,"");
+  strcat(sendline, "select * from classics");
+  send(sockfd, sendline, strlen(sendline), 0);		
+  if (recv(sockfd, recvline, MAXLINE,0) == 0){
+   //error: server terminated prematurely
+   perror("The server terminated prematurely"); 
+   exit(4);
+  }
+  fprintf(f, "%s\n", recvline);
+  fputs(recvline, stdout);
+  printf("\n");
+  sleep(2);
+  strcpy(sendline,"");
+  fclose(f);
+  f = fopen("a4Client2Log.txt", "a");
+ }
+ fclose(f);
+ exit(0);
+}
---
+++ a4Server3.c
@@ -0,0 +1,168 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <sqlite3.h> 
+#include <stdlib.h>
+#include <string.h>
+
+
+/*
+    CONCURRENT SERVER: THREAD EXAMPLE
+    Must be linked with the "pthread" library also, e.g.:
+       cc -o example example.c -lnsl -lsocket -lpthread 
+
+    This program creates a connection socket, binds a name to it, then
+    listens for connections to the sockect.  When a connection is made,
+    it accepts messages from the socket until eof, and then waits for
+    another connection...
+
+    This is an example of a CONCURRENT server -- by creating threads several
+    clients can be served at the same time...
+
+    This program has to be killed to terminate, or alternately it will abort in
+    120 seconds on an alarm...
+*/
+
+
+struct serverParm {
+           int connectionDesc;
+       };
+       
+void *serverThread(void *parmPtr) {
+
+#define PARMPTR ((struct serverParm *) parmPtr)
+    int recievedMsgLen;
+    char messageBuf[1025];
+   	char *zErrMsg = 0;
+	  int rc;
+    sqlite3 *db;
+    sqlite3_stmt *statement;   
+    FILE *f;
+    time_t t;
+    time(&t);
+	  /* Open database */
+  	rc = sqlite3_open("book.db", &db);
+  	if( rc )
+  	{
+	  	fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+	  	exit(0);
+  	}
+	  else
+	  {
+  		fprintf(stderr, "Opened database successfully\n");
+	  }
+
+    /* Server thread code to deal with message processing */
+    printf("DEBUG: connection made, connectionDesc=%d\n",
+            PARMPTR->connectionDesc);
+    if (PARMPTR->connectionDesc < 0) {
+        printf("Accept failed\n");
+        return(0);    /* Exit thread */
+    }
+    
+    /* Receive messages from sender... */
+    while ((recievedMsgLen= read(PARMPTR->connectionDesc,messageBuf,sizeof(messageBuf)-1)) > 0) 
+    {
+        //recievedMsgLen[messageBuf] = '\0';
+        messageBuf[recievedMsgLen] = '\0';
+        char result[1025] = "";
+    if ( sqlite3_prepare(db, messageBuf, -1, &statement, 0 ) == SQLITE_OK ) 
+	  {
+		  int ctotal = sqlite3_column_count(statement);
+		  int res = 0;
+      f = fopen("a4p3ServerLog.txt", "a");
+      printf("Process ID : %d, Thread ID: %d\n", getpid(), pthread_self());
+      printf("Processing: %s\n", messageBuf);   
+      fprintf(f, "Process ID : %d, Thread ID: %d\n", getpid(), pthread_self());
+      fprintf(f, "Processing: %s\n", messageBuf);
+      fclose(f);
+		  while ( 1 )         
+		  {      
+			  res = sqlite3_step(statement);
+			  if ( res == SQLITE_ROW ) 
+			  {
+           int i ;
+  	      for(i = 0; i < ctotal; i++)
+          {
+              if(i == ctotal - 1)
+              {
+                strcat(result, (char*)sqlite3_column_text(statement, i));
+                strcat(result, "\n");
+              }
+              else
+              {
+                strcat(result, (char*)sqlite3_column_text(statement, i)); 
+                strcat(result, " | ");
+              }
+          }
+             
+			  }										
+			  if ( res == SQLITE_DONE || res==SQLITE_ERROR)                                                                              
+			  {
+			  	break;                                                                                                                     
+			  }   
+	  	}																	
+	  }
+        if (write(PARMPTR->connectionDesc,result,1025) < 0) {
+               perror("Server: write error");
+               return(0);
+           }
+    }
+    sqlite3_close(db);
+    close(PARMPTR->connectionDesc);  /* Avoid descriptor leaks */
+    free(PARMPTR);                   /* And memory leaks */
+    return(0);                       /* Exit thread */
+}
+
+int main (int argc, char **argv) {
+    int listenDesc;
+    struct sockaddr_in myAddr;
+    struct serverParm *parmPtr;
+    int connectionDesc;
+    pthread_t threadID;
+    FILE *f;
+    f = fopen("a4p3ServerLog.txt", "w");
+    fclose(f);
+    /* For testing purposes, make sure process will terminate eventually */
+    alarm(120);  /* Terminate in 120 seconds */
+
+    /* Create socket from which to read */
+    if ((listenDesc = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("open error on socket");
+        exit(1);
+    }
+
+    /* Create "name" of socket */
+    myAddr.sin_family = AF_INET;
+    myAddr.sin_addr.s_addr = INADDR_ANY;
+    myAddr.sin_port = htons((int) strtol(argv[1], (char **)NULL, 10));
+    //myAddr.sin_port = htons(PORTNUMBER);
+        
+    if (bind(listenDesc, (struct sockaddr *) &myAddr, sizeof(myAddr)) < 0) {
+        perror("bind error");
+        exit(1);
+    }
+
+    /* Start accepting connections.... */
+    /* Up to 5 requests for connections can be queued... */
+    listen(listenDesc,5);
+    while (1) /* Do forever */ {
+        /* Wait for a client connection */
+        connectionDesc = accept(listenDesc, NULL, NULL);
+
+        /* Create a thread to actually handle this client */
+        parmPtr = (struct serverParm *)malloc(sizeof(struct serverParm));
+        parmPtr->connectionDesc = connectionDesc;
+        if (pthread_create(&threadID, NULL, serverThread, (void *)parmPtr) 
+              != 0) {
+            perror("Thread create error");
+            close(connectionDesc);
+            close(listenDesc);
+            exit(1);
+        }
+        printf("Parent ready for another connection\n"); 
+    }
+
+}
